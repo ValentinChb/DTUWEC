4234d2e2f8e60c71b71878b8d201ce9dad73b81b
ValentinChb <valentin.chabaud@sintef.no>
2022-06-07 11:33:54 +0200

    Updated cmake

--- custom-build/src/dtu_we_controller/version.h
+++ custom-build/src/dtu_we_controller/version.h
@@ -1,7 +1,7 @@
-#define GIT_ID "0.5-24-g46a0ede-dirty" 
+#define GIT_ID "0.5-25-g7803fa2-dirty" 
 #define GIT_VERSION 0,5,-1 
 #define GIT_BRANCH "master" 
 #define USERNAME "valentinc" 
 #define COMPUTERNAME "SINTEFPC8945" 
-#define BUILD_DATE "31.05.2022" 
+#define BUILD_DATE "07.06.2022" 
 #define BUILD_TYPE "Windows64 release" 
